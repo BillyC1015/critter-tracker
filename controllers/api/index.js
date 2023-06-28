19addf892d2feb464b7080a763f2c76ab5634aee
Billy Cody <william.cody1015@gmail.com>
2023-06-28 17:19:38 -0400

    fixed merge problem

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const seedDbRoute = require("./seedDB");
 const animalRoutes = require("./animalRoutes")
+const mapRoute = require("./maps-api")
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/seedDb', seedDbRoute);
