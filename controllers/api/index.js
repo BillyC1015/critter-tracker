76b0b00ed5b243cb68c0d8b1af72c1ed200efa97
Fawn <sarahmargaret@ymail.com>
2023-06-27 22:00:11 -0400

    map renders on page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const seedDbRoute = require("./seedDB");
-
+const mapRoute = require('./maps-api');
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/seedDb', seedDbRoute);
-
+router.use('/map', mapRoute);
 module.exports = router;
