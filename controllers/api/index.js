cb84fd18fa176b2b3e182d55182605fc8b783eb1
Billy Cody <william.cody1015@gmail.com>
2023-06-28 17:24:15 -0400

    still trying to figure out merge conflict

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 const seedDbRoute = require("./seedDB");
-const animalRoutes = require("./animalRoutes")
-const mapRoute = require("./maps-api")
+const animalRoutes = require("./animalRoutes");
+// const mapRoute = require("./maps-api");
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
 router.use('/seedDb', seedDbRoute);
