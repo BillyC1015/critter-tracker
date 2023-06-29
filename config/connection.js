386ac4c8ec17a26585b77adf775fb1aa7a81f3e6
asiabeoon <a.hashbey@gmail.com>
2023-06-29 18:30:53 -0400

    stareted work on homepage

--- config/connection.js
+++ config/connection.js
@@ -11,11 +11,12 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      port: 3306
+      port:3306
     }
   );
 }
+
 
 module.exports = sequelize;
