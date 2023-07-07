e15500fafd5869fa56ecfe636bb9ae9d81e332c9
Billy Cody <william.cody1015@gmail.com>
2023-07-06 22:03:09 -0400

    heroku issue still

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on http://127.0.0.1:' + PORT));
 });
