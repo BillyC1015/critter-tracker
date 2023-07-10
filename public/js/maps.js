d5e59758fa7b0861f502d5b8c0b05618991b0af1
Tyler <t.woods95@aol.com>
2023-07-09 21:40:40 -0400

    Updated more animals to map

--- public/js/maps.js
+++ public/js/maps.js
@@ -33,34 +33,41 @@ map = L.mapquest.map('map', {
 // var marker = L.marker([40.511460, -75.559543]).addTo(map);
 // marker.bindPopup("<a href='/animalProfile/1'>Steven</a>.").openPopup();
 
+// New York
 var marker = L.marker([40.7128, -74.0060]).addTo(map);
 marker.bindPopup(`
-<a class="animal-link" href='/animalProfile/1'>Blue bird</a>
+<a class="animal-link" href='/animalProfile/1'>Blue Bird</a>
 <br/>
 `).openPopup();
 
+// Ottawa
 var marker = L.marker([45.4215, -75.6972]).addTo(map);
 marker.bindPopup(`
 <a class="animal-link" href='/animalProfile/2'>Cardinal</a>
 <br/>
 `).openPopup();
 
+// Virginia
+var marker = L.marker([37.2710, -79.9414]).addTo(map);
+marker.bindPopup(`
+<a class="animal-link" href='/animalProfile/3'>Robin</a>
+<br/>
+`).openPopup();
 
-var marker = L.marker([41.511460, -75.559543]).addTo(map);
+// Seattle, Washington
+var marker = L.marker([47.6062, -122.3321]).addTo(map);
 marker.bindPopup(`
 <a class="animal-link" href='/animalProfile/5'>Woodpecker</a>
 <br/>
-<a class="animal-link" href='/animalProfile/4'>Robin</a>
 `).openPopup();
 
-
+// Philadelphia, PA
 var marker = L.marker([39.9526, -75.1652]).addTo(map);
 marker.bindPopup(`
 <a class="animal-link" href='/animalProfile/6'>Canary</a>
 <br/>
 <a class="animal-link" href='/animalProfile/7'>Eagle</a>
 `).openPopup();
-
 
 }
 
