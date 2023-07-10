a7e48f4949ddda6a3b2b11d4c772b8c26cc58a03
Tyler <t.woods95@aol.com>
2023-07-09 23:18:57 -0400

    Fixed hamburger menu for mobile

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 
-const PORT = process.env.PORT || 7878;
+const PORT = process.env.PORT || 3001;
 
 
 // Set up Handlebars.js engine with custom helpers
